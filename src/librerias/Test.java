fa32492a44b7ab49bad42b7a3d7d0e5b2e8828f5
Charly Cimino <you@example.com>
2021-03-20 21:03:05 -0300

    Clase Consola

---
+++ src/librerias/Test.java
@@ -0,0 +1,17 @@
+package librerias;
+
+/**
+ *
+ * @author Charly Cimino Aprendé más Java en mi canal:
+ * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
+ * GitHub: https://github.com/caemci
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        String nombre = Consola.leer("¿Cuál es tu nombre?");
+        Consola.escribir("Hola " + nombre);
+
+    }
+
+}
