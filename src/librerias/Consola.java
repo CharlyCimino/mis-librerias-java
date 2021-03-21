f7baf47691cb020c28a308510951d285b90b9a1e
Charly Cimino <you@example.com>
2021-03-20 21:04:26 -0300

    Cambio de link para GitHub

--- src/librerias/Consola.java
+++ src/librerias/Consola.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
  *
  * @author Charly Cimino Aprendé más Java en mi canal:
  * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
- * GitHub: https://github.com/caemci
+ * GitHub: https://github.com/CharlyCimino
  */
 public class Consola {
 
--- src/librerias/Test.java
+++ src/librerias/Test.java
@@ -4,7 +4,7 @@ package librerias;
  *
  * @author Charly Cimino Aprendé más Java en mi canal:
  * https://www.youtube.com/c/CharlyCimino Encontrá más código en mi repo de
- * GitHub: https://github.com/caemci
+ * GitHub: https://github.com/CharlyCimino
  */
 public class Test {
 
